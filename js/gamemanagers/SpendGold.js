069d6dcf0dcaff37f941d3b3b7c07f4fb2c4d038
nareg-agopian <nareg19@outlook.com>
2015-04-28 14:56:23 -0700

    no message

--- js/gamemanagers/SpendGold.js
+++ js/gamemanagers/SpendGold.js
@@ -1,77 +1,3 @@
-game.GameTimerManager = Object.extend({
-    init: function(x, y, settings) {
-        this.now = new Date().getTime();
-        this.lastCreep = new Date().getTime();
-        this.paused = false;
-        this.alwaysUpdate = true;
-    },
-    update: function() {
-        this.now = new Date().getTime();
-        this.goldTimerCheck(this.now);
-        this.creepTimerCheck();
-
-        return true;
-    },
-    goldTimerCheck: function() {
-        if (Math.round(this.now / 1000) % 10 === 0 && (this.now - this.lastCreep >= 1000)) {
-            game.data.gold += 1;
-            console.log("Current gold: " + game.data.gold);
-        }
-    },
-    creepTimerCheck: function() {
-        if (Math.round(this.now / 1000) % 10 === 0 && (this.now - this.lastCreep >= 1000)) {
-            this.lastCreep = this.now;
-            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-            me.game.world.addChild(creepe, 5);
-        }
-    }
-});
-
-game.HerodeathManager = Object.extend({
-    init: function(x, y, settings) {
-        this.alwaysUpdate = true;
-    },
-    update: function() {
-        if (game.data.player.dead) {
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
-
-        return true;
-    }
-});
-
-game.ExperienceManager = Object.extend({
-    init: function(x, y, settings) {
-        this.alwaysUpdate = true;
-        this.gameover = false;
-        return true;
-    },
-    update: function() {
-        if (game.data.win === true && !this.gameover) {
-            this.gameOver(true);
-        } else if (game.data.win === false && !this.gameover) {
-            this.gameOver(false);
-        }
-
-        return true;
-    },
-    gameOver: function(win) {
-        if (win) {
-            game.data.exp += 10;
-        } else {
-            game.data.exp += 1;
-        }
-
-        game.data.exp += 10;
-        this.gameover = true;
-        me.save.exp = game.data.exp;
-        //For testing purposes only
-        me.save.exp2 = 4;
-    }
-
-});
-
 game.SpendGold = Object.extend({
     init: function(x, y, settings) {
         this.now = new Date().getTime();
@@ -222,3 +148,6 @@ game.SpendGold = Object.extend({
         }
 
     }});
+
+
+
